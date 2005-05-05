9f7ce11020feb63f0175b1a1e03238559af59bf2
tsdgeos <tsdgeos@db748b10-b2f0-0310-8f33-9d55c856353e>
2005-05-05 13:42:40 +0000

    A bit better autogen

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -1,5 +1,18 @@
-aclocal
-autoheader
-libtoolize --automake
-automake --add-missing
-autoconf
+#!/bin/sh
+
+run ()
+{
+    echo "running: $*"
+    eval $*
+
+    if test $? != 0 ; then
+	echo "error: while running '$*'"
+	exit 1
+    fi
+}
+
+run aclocal
+run autoheader
+run libtoolize --automake
+run automake --add-missing
+run autoconf
