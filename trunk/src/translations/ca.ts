b28106a57ff66f469c8bc90329274d6cd7a2b1c3
tsdgeos <tsdgeos@db748b10-b2f0-0310-8f33-9d55c856353e>
2005-05-09 16:31:56 +0000

    The files i always forget to commit

---
+++ trunk/src/translations/ca.ts
@@ -0,0 +1,60 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name></name>
+    <message>
+        <source>File %1 does not exist</source>
+        <translation>El fitxer %1 no existeix</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+</context>
+<context>
+    <name>Language</name>
+    <message>
+        <source>English</source>
+        <comment>Here put the name of the language you are translating. Example: If you are doing the Spanish Translation write Espa&#xc3;&#xb1;ol</comment>
+        <translation>Català</translation>
+    </message>
+</context>
+<context>
+    <name>glWidget</name>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>Could not open the map, check you have read permission on %1</source>
+        <translation>No s&apos;ha pogut obrir el mapa, comproveu que tingueu permís de lectura a %1</translation>
+    </message>
+    <message>
+        <source>There is no parser available for that kind of file.</source>
+        <translation>No hi ha cap parser per aquest tipus de fitxer.</translation>
+    </message>
+    <message>
+        <source>&amp;Open</source>
+        <translation>&amp;Obre</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Tanca</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Abandona</translation>
+    </message>
+    <message>
+        <source>Languages</source>
+        <translation type="obsolete">Idiomes</translation>
+    </message>
+    <message>
+        <source>DEM files (*.dem);;Plain maps (*.pm)</source>
+        <translation>Fitxers DEM (*.dem);;Mapes simples (*.pm)</translation>
+    </message>
+    <message>
+        <source>&amp;Languages</source>
+        <translation type="unfinished">&amp;Idiomes</translation>
+    </message>
+</context>
+</TS>
---
+++ trunk/src/translations/template.ts
@@ -0,0 +1,56 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name></name>
+    <message>
+        <source>File %1 does not exist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Language</name>
+    <message>
+        <source>English</source>
+        <comment>Here put the name of the language you are translating. Example: If you are doing the Spanish Translation write Espa&#xc3;&#xb1;ol</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>glWidget</name>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not open the map, check you have read permission on %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>There is no parser available for that kind of file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DEM files (*.dem);;Plain maps (*.pm)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Languages</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
