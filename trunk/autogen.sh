dec6cc0d1ba1c427297c9779531d9b33fa3076e5
tsdgeos <tsdgeos@db748b10-b2f0-0310-8f33-9d55c856353e>
2005-07-16 00:09:50 +0000

    warn about automake 1.4 not beign supported

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+automakeVersion=`automake --version`
+result=`expr match "$automakeVersion" ".* 1\.4"`
+if [ $result != 0 ]; then
+	echo "r3v does not work with automake 1.4"
+	echo "Either install a newer automake version"
+	echo "or if you already have one modify this file"
+	echo "and change"
+	echo "automake --add-missing"
+	echo "to"
+	echo "automake-YOURVERSION --add-missing"
+	exit
+fi
+	
+
 run ()
 {
     echo "running: $*"
